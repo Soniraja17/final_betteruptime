aad436c927e98b00683c15c8ca2647dbd9d71647
Soniraja17 <your.email@example.com>
2025-07-20 14:33:05 +0530

    minor changes

--- apps/fe/lib/utils.ts
+++ apps/fe/lib/utils.ts
@@ -6,12 +6,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// export const BACKEND_URL="https://final-betteruptime.onrender.com"
+export const BACKEND_URL=process.env.BACKEND_URI
 
-const BACKEND_URL=axios.create({
-  baseURL:process.env.BACKEND_URI
-})
+// const BACKEND_URL=axios.create({
+//   baseURL:process.env.BACKEND_URI
+// })
 
-export default BACKEND_URL;
+// export default BACKEND_URL;
 
 // export const BACKEND_URL="http://localhost:3000"
