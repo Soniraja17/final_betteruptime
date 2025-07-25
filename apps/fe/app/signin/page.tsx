817e0511000ae3c61e7dd830d1350c075d1044ab
Soniraja17 <your.email@example.com>
2025-07-25 18:18:09 +0530

    change name in layout

--- apps/fe/app/signin/page.tsx
+++ apps/fe/app/signin/page.tsx
@@ -134,9 +134,13 @@ function SignIn() {
               </div>
 
               <button
+               onClick={()=>{
+                alert("Wait for backend to render....")
+               }}
                 type="submit"
                 className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg font-semibold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition-colors"
               >
+                
                 Sign In
               </button>
             </form>
